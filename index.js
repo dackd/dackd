52cc51d6a9d61e6e6dceb5efa1774265bdd58444
dackd <dackd@hblab.vn>
2025-06-03 15:42:14 +0700

    add favicon

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
